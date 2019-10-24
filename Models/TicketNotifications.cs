f3f74a07f1ec6fcee325b5cc0c14ed1b69b64bd2
Brandon M. Beck <55066809+bbeck7412@users.noreply.github.com>
2019-10-23 21:22:47 -0400

    Initial commit

---
+++ Models/TicketNotifications.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class TicketNotification
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public string UserId { get; set; }
+        public string NotificationBody { get; set; }
+        public bool IsRead { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+    }
+}
