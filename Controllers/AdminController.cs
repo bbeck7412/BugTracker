1ef7caacb01dfd188100ea4588914cfb46a0acff
Brandon M. Beck <55066809+bbeck7412@users.noreply.github.com>
2019-10-27 22:52:19 -0400

    Update

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult ManageRoles()
+        {
+            ViewBag.Users = new MultiSelectList(db.Users, "Id","Email");
+            ViewBag.Role = new SelectList(db.Roles,"Name","Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ManageRoles()
+        {
+            return View();
+        }
+    }
+}
