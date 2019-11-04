09d991bddce23bb653b9c4f195b09d5511278613
Brandon M. Beck <55066809+bbeck7412@users.noreply.github.com>
2019-11-03 21:21:01 -0500

    Update

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -160,12 +160,9 @@ namespace BugTracker.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser
-                {
-                    UserName = model.Email,
-                    Email = model.Email,
-                    FirstName = model.FirstName,
-                    LastName = model.LastName
+                var user = new ApplicationUser { UserName= model.Email,
+                    FirstName= model.FirstName, LastName = model.LastName,
+                    Email = model.Email, DisplayName = $"{model.FirstName} {model.LastName}" 
                 };
 
                 var result = await UserManager.CreateAsync(user, model.Password);
